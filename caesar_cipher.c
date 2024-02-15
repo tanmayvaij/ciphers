62ede9e0ee196bb4183f63ef5182c20a82d64c67
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-02-15 18:58:19 +0530

    added caesar cipher decryption code
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- caesar_cipher.c
+++ caesar_cipher.c
@@ -1,26 +1,44 @@
+// Caesar Cipher Code
+
 #include <stdio.h>
 #include <string.h>
 
-void encrypt(char *plainString, int n)
+void encrypt(char *plainText, int key)
 {
-    for (int i = 0; i < strlen(plainString); i++)
+    for (int i = 0; i < strlen(plainText); i++)
     {
-        if (plainString[i] >= 65 && plainString[i] <= 91)
-        {
-            plainString[i] = ((plainString[i] - 65 + n) % 26) + 65;
-        }
-        else if (plainString[i] >= 97 && plainString[i] <= 123)
-        {
-            plainString[i] = ((plainString[i] - 97 + n) % 26) + 97;
-        }
+        if (plainText[i] >= 65 && plainText[i] <= 91)
+            plainText[i] = ((plainText[i] - 65 + key) % 26) + 65;
+
+        else if (plainText[i] >= 97 && plainText[i] <= 123)
+            plainText[i] = ((plainText[i] - 97 + key) % 26) + 97;
+    }
+}
+
+void decrypt(char *cipherText, int key)
+{
+    for (int i = 0; i < strlen(cipherText); i++)
+    {
+        if (cipherText[i] >= 65 && cipherText[i] <= 91)
+            cipherText[i] = ((cipherText[i] - 65 - key + 26) % 26) + 65;
+
+        else if (cipherText[i] >= 97 && cipherText[i] <= 123)
+            cipherText[i] = ((cipherText[i] - 97 - key + 26) % 26) + 97;
     }
 }
 
 int main()
 {
-    char plainString[] = "abcdefghijklmnopqrstuvwxyz";
-    encrypt(plainString, 5);
-    printf("%s", plainString);
+    char text[] = "tanmayvaij";
+    int key = 5;
+
+    encrypt(text, key);
+
+    printf("encryted text: %s \n", text);
+
+    decrypt(text, key);
+
+    printf("decrypted text: %s \n", text);
 
     return 0;
 }
